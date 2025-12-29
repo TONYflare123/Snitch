e74cfa4b504242040a7060e646babc48cddfe52d
Flarium <132795234+TONYflare123@users.noreply.github.com>
2025-12-29 18:08:33 +0530

    Add navigation bar to the top of the page

---
+++ Nav.js
@@ -0,0 +1,7 @@
+document.body.insertAdjacentHTML('afterbegin', `
+  <div id="snitch-nav" style="background: black; color: lime; padding: 10px; font-family: monospace; border-bottom: 2px solid lime;">
+    <strong>THE SNITCH DIGITAL</strong> | 
+    <a href="index.html" style="color: lime;">HOME</a> | 
+    <a href="tips.html" style="color: lime;">SUBMIT A TIP</a>
+  </div>
+`);
